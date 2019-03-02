8cd7874938b383ac10d3e6d04fcb7edc97ee27aa
root <aikanshgarg0303@gmail.com>
2019-03-02 19:39:37 +0530

    Add & Edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ProductItem from './ProductItem';
+import AddItem from './AddItem';
 
 const products = [
   {
@@ -23,6 +24,8 @@ class App extends Component {
       products: JSON.parse(localStorage.getItem('products'))
     };
 
+    this.onAdd = this.onAdd.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
     this.onDelete = this.onDelete.bind(this); // binding the method to delete button
   }
 
@@ -39,6 +42,17 @@ class App extends Component {
     //console.log(products);    
   }
 
+  onAdd(name, price) {
+      const products = this.getProducts();
+
+      products.push({ // add data to local storage
+        name,
+        price
+      });
+
+       this.setState({ products }); // reflect on UI
+  }
+
   onDelete(name) {
     //console.log(name);
     const products = this.getProducts();
@@ -50,12 +64,31 @@ class App extends Component {
     this.setState({ products: filteredProducts })
   }
 
+  onEditSubmit(name, price, originalName) {
+    let products = this.getProducts(); // mutating the products so let used, instead of const
+
+    products = products.map(product => {
+      if(product.name === originalName) {
+        product.name = name;
+        product.price = price;
+      }
+
+      return product;
+    });
+
+    this.setState({ products });
+  }
+
   render() {
     return (
       <div className="App">
-
-       
         <h1>Products Manager</h1>
+
+
+        <AddItem
+          onAdd={this.onAdd}
+        />
+
         {
           this.state.products.map(product => {
             return (
@@ -65,6 +98,7 @@ class App extends Component {
                 price={product.price}
 
                 onDelete={this.onDelete}
+                onEditSubmit={this.onEditSubmit}
 
               />  
             );
