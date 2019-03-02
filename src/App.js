8dfb36883a4243754032327895afccc74e53e380
root <aikanshgarg0303@gmail.com>
2019-03-02 17:12:10 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const products = [
+  {
+    name: 'IPad',
+    price: 200
+  },
+  {
+    name: 'iPhone',
+    price: 650
+  }
+];
+
+localStorage.setItem('products',)
+
 class App extends Component {
+  onChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
+    const title = 'This is Michael!';
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+       
+        <h1>{title}</h1>
+        <input onChange={this.onChange}/>
+
       </div>
     );
   }
